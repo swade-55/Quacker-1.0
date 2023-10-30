f08423ce03075f40fdc143f104b6787de4db6d2e
PraneethSomisetty1 <psomiset@calpoly.edu>
2023-10-29 19:53:09 -0500

    login/signup/edit-index.js
    added login/sign up screens, and made index.js class based to import firebase to it and use it to fetch.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,109 @@
- import { db } from "../Firebase.js"
- import { doc, addDoc, getDocs, collection,increment } from "firebase/firestore";
+//  import { db } from "../Firebase.js"
+//  import { doc, addDoc, getDocs, collection,increment } from "firebase/firestore";
 
-let addQuack = false;
+// let addQuack = false;
 
-function fetchQuacks(){
-  console.log("Fetching quacks")
-  return getDocs(collection(db, "Quacks"))
-  .then(res=>res.json())
-  .then(data=>renderQuacks(data))
-};
+// function fetchQuacks(){
+//   console.log("Fetching quacks")
+//   return getDocs(collection(db, "Quacks"))
+//   .then(res=>res.json())
+//   .then(data=>renderQuacks(data))
+// };
 
 
 
-function renderQuacks(quacks){
+// function renderQuacks(quacks){
+//   const quackCollection = document.getElementById('quack-collection');
+//   quacks.forEach(quack=>{
+//     const quackDiv = document.createElement('div');
+//     const quackH2 = document.createElement('h2');
+//     const quackContent = document.createElement('p');
+//     const quackButton = document.createElement('button');
+//     quackButton.addEventListener('click',(e)=>{
+//       updateDoc(doc(db, "Quacks", quack.id), {
+//         Likes: increment(1),
+//       })
+//       .then((data)=>data.json())
+//       .then(res => {
+//         quack.likes = res.likes
+//         quackLikes.textContent = res.likes
+//    })   
+//     })
+//     const quackLikes = document.createElement('p');
+//     quackLikes.textContent = quack.likes;
+//     quackButton.textContent = 'Like';
+//     quackButton.classList.add('like-btn');
+//     quackButton.setAttribute('id',quack.id);
+//     quackH2.textContent = quack.name;
+//     quackDiv.appendChild(quackH2);
+//     quackContent.textContent = quack.postContent
+//     quackDiv.appendChild(quackContent);
+//     quackDiv.appendChild(quackButton);
+//     quackDiv.appendChild(quackLikes);
+//     quackDiv.classList.add('card');
+//     quackCollection.appendChild(quackDiv);
+//   })
+// }
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   fetchQuacks();
+//   const addBtn = document.querySelector("#new-quack-btn");
+//   const quackFormContainer = document.querySelector(".container");
+//   addBtn.addEventListener("click", () => {
+//     // hide & seek with the form
+//     addQuack = !addQuack;
+//     if (addQuack) {
+//       quackFormContainer.style.display = "block";
+//     } else {
+//       quackFormContainer.style.display = "none";
+//     }
+//   });
+// });
+
+
+// let createQuack = document.getElementsByClassName('add-quack-form')[0];
+// createQuack.addEventListener('submit',(e)=>{
+//   e.preventDefault()  
+//   let quackObject = {
+//     name: document.getElementsByClassName('add-quack-form')[0].childNodes[3].value,
+//     postContent: document.getElementsByClassName('add-quack-form')[0].childNodes[7].value,
+//     likes: 0,
+//   }
+//   addNewQuack(quackObject);
+// });
+
+
+// function addNewQuack(quackObject){
+//   return fetch(`http://localhost:3000/quacks`,{
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(quackObject)
+//   })
+//   .then(res=>res.json())
+//   .then(quack=>renderQuacks([quack]))
+// }
+
+import { db } from "./Firebase.js";
+import { doc, addDoc, getDocs, collection, increment } from "firebase/firestore";
+
+class QuackApi {
+  constructor() {
+    this.addQuack = false;
+  }
+
+  fetchQuacks() {
+    console.log("Fetching quacks");
+    return getDocs(collection(db, "Quacks"))
+      .then((snapshot) => {
+        const quacks = snapshot.docs.map((doc) => doc.data());
+        this.renderQuacks(quacks);
+      });
+  }
+
+  renderQuacks(quacks) {
   const quackCollection = document.getElementById('quack-collection');
   quacks.forEach(quack=>{
     const quackDiv = document.createElement('div');
@@ -43,45 +134,50 @@ function renderQuacks(quacks){
     quackDiv.classList.add('card');
     quackCollection.appendChild(quackDiv);
   })
-}
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  fetchQuacks();
-  const addBtn = document.querySelector("#new-quack-btn");
-  const quackFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addQuack = !addQuack;
-    if (addQuack) {
-      quackFormContainer.style.display = "block";
-    } else {
-      quackFormContainer.style.display = "none";
-    }
-  });
-});
-
-
-let createQuack = document.getElementsByClassName('add-quack-form')[0];
-createQuack.addEventListener('submit',(e)=>{
-  e.preventDefault()  
-  let quackObject = {
-    name: document.getElementsByClassName('add-quack-form')[0].childNodes[3].value,
-    postContent: document.getElementsByClassName('add-quack-form')[0].childNodes[7].value,
-    likes: 0,
   }
-  addNewQuack(quackObject);
-});
 
-
-function addNewQuack(quackObject){
-  return fetch(`http://localhost:3000/quacks`,{
+  addNewQuack(quackObject) {
+    return fetch(`http://localhost:3000/quacks`,{
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(quackObject)
-  })
-  .then(res=>res.json())
-  .then(quack=>renderQuacks([quack]))
+    })
+    .then(res=>res.json())
+    .then(quack=>renderQuacks([quack]))
+  }
+
+  // Other methods can go here
+
+  init() {
+    document.addEventListener("DOMContentLoaded", () => {
+      this.fetchQuacks();
+      const addBtn = document.querySelector("#new-quack-btn");
+      const quackFormContainer = document.querySelector(".container");
+      addBtn.addEventListener("click", () => {
+        this.addQuack = !this.addQuack;
+        if (this.addQuack) {
+          quackFormContainer.style.display = "block";
+        } else {
+          quackFormContainer.style.display = "none";
+        }
+      });
+    });
+
+    let createQuack = document.getElementsByClassName('add-quack-form')[0];
+    createQuack.addEventListener('submit', (e) => {
+      e.preventDefault();
+      let quackObject = {
+        name: document.getElementsByClassName('add-quack-form')[0].childNodes[3].value,
+        postContent: document.getElementsByClassName('add-quack-form')[0].childNodes[7].value,
+        likes: 0,
+      };
+      this.addNewQuack(quackObject);
+    });
+  }
 }
+
+const quackApi = new QuackApi();
+quackApi.init();
+
